472a652784d4757d97d7bf2942a780e65aa65cce
Mohammad-Saad-eng <56753379+Mohammad-Saad-eng@users.noreply.github.com>
2021-08-14 12:48:16 +0500

    Add files via upload

---
+++ Pose Detector.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import math
+import cv2
+import numpy as np
+from time import time
+import mediapipe as mp
+import matplotlib.pyplot as plt
+
+# Initializing mediapipe pose class.
+mp_pose = mp.solutions.pose
+
+# Setting up the Pose function.
+pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.3, model_complexity=2)
+
+# Initializing mediapipe drawing class, useful for annotation.
+mp_drawing = mp.solutions.drawing_utils 
+
+def detectPose(image, pose, display=True):
+    output_image = image.copy()
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    results = pose.process(imageRGB)
+    
+    height, width, _ = image.shape
+    
+    landmarks = []
+    
+    if results.pose_landmarks:
+        mp_drawing.draw_landmarks(image=output_image, landmark_list=results.pose_landmarks, connections=mp_pose.POSE_CONNECTIONS)
+        
+        for landmark in results.pose_landmarks.landmark:
+            landmarks.append((int(landmark.x*width),(int(landmark.y*height)),(landmark.z*width)))
+
+    if display:
+        plt.figure(figsize=[22,22])
+        plt.subplot(121);plt.imshow(image[:,:,::-1]);plt.title("Original Image");plt.axis('off');
+        plt.subplot(122);plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
+        
+        mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+    
+    else:
+        return output_image, landmarks
+    
+image = cv2.imread(r"D:\OpenCV\Introduction to Pose Detection and Pose Classification\media\kick.jpg")
+detectPose(image,pose)
+
+
+# In[ ]:
+
+
+
+
